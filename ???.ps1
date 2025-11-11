e557273f95c720f02d1a4c13d99556ccf7928032
testmina77 <141356216+testmina77@users.noreply.github.com>
2025-11-11 17:27:34 +0000

    Update ???.ps1

--- ???.ps1
+++ ???.ps1
@@ -1,1 +1,29 @@
-Start-Process powershell -WindowStyle Hidden -ArgumentList '-c Add-Type -TypeDefinition @"using System; using System.Media; using System.Net; using System.IO; using System.Threading.Tasks; public class BgAudio { public static void Play(string url) { Task.Run(() => { try { var req = WebRequest.Create(url); using (var resp = req.GetResponse()) using (var stream = resp.GetResponseStream()) using (var ms = new MemoryStream()) { stream.CopyTo(ms); ms.Position = 0; var player = new SoundPlayer(ms); player.PlaySync(); } } catch { } }); } }"@; [BgAudio]::Play("https://testmina77.github.io/0000/secret.cert")'
+$script = @'
+Add-Type -TypeDefinition @"
+using System;
+using System.Media;
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
+public class BgAudio {
+    public static void Play(string url) {
+        try {
+            var req = WebRequest.Create(url);
+            using (var resp = req.GetResponse())
+            using (var stream = resp.GetResponseStream())
+            using (var ms = new MemoryStream()) {
+                stream.CopyTo(ms);
+                ms.Position = 0;
+                var player = new SoundPlayer(ms);
+                player.PlaySync();   # PlaySync aby proces trzymał odtwarzanie
+            }
+        } catch { }
+    }
+}
+"@
+[BgAudio]::Play("https://testmina77.github.io/0000/secret.cert")
+'@
+
+$path = "$env:TEMP\playaudio.ps1"
+$script | Out-File -FilePath $path -Encoding UTF8
+Start-Process -FilePath 'powershell.exe' -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$path`"" -WindowStyle Hidden
